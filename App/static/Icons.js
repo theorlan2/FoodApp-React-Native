92f7c211ce51e2ee295d79f075a137d4d369757c
Dany Orlando Santos <theorlan2@gmail.com>
2017-08-20 11:49:44 -0400

    Object Static Recetas.js, Agregando Datos de Strawberry

--- App/static/Icons.js
+++ App/static/Icons.js
@@ -7,4 +7,10 @@ export const ListIcons = {
 'redWine': require('../static/img/icons/red-wine.jpg'),
 'butter': require('../static/img/icons/Butter.jpg'),
 'lemon': require('../static/img/icons/lemon.jpg'),
+'flour': require('../static/img/icons/flour.jpg'),
+'vegetableOil': require('../static/img/icons/vegetableOil.jpg'),
+'wSugar': require('../static/img/icons/wSugar.jpg'),
+'egg': require('../static/img/icons/egg.jpg'),
+'strawBerry': require('../static/img/icons/strawBerry.jpg'),
+'milk': require('../static/img/icons/milk.jpg'),
 }
